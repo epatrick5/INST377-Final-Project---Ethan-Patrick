7928b56b8e16e07b53ae0992e132614ea228d284
Ethan-Patrick5 <epatrick504@gmail.com>
2026-05-17 11:13:47 -0400

    Added final comments to js files and fixed some spacing

--- wisewagering/api/bets.js
+++ wisewagering/api/bets.js
@@ -1,9 +1,11 @@
+// Import the Supabase client to interact with the database
 import { createClient } from "@supabase/supabase-js";
-
+// Create the client that holds the supabase URL and the public API key for authentication
 const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_KEY
 );
+// Gets the bets from the database 
 export default async function handler(req, res) {
   if (req.method === "GET") {
     const { data, error } = await supabase
@@ -16,6 +18,7 @@ export default async function handler(req, res) {
     }
     return res.status(200).json(data);
   }
+// Sends the bets to the database after a user clicks on a bet
   if (req.method === "POST") {
     const { sport, team, odds, bet_type } = req.body;
     const { data, error } = await supabase
--- wisewagering/api/mlb.js
+++ wisewagering/api/mlb.js
@@ -3,8 +3,6 @@ export default async function handler(req, res) {
   const response = await fetch(
     `https://api.the-odds-api.com/v4/sports/baseball_mlb/odds/?apiKey=${process.env.API_KEY}&regions=us&markets=h2h,spreads,totals&oddsFormat=american`
   );
-
   const data = await response.json();
-
   res.status(200).json(data);
 }
